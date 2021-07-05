af6764121ad87a5fc48ccc2d4aedc79e6bc4c11b
Isabella18081996 <Isabella.Nita@yahoo.it>
2021-07-05 09:03:15 +0200

    controlli form

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
 use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -37,10 +38,18 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
         $data = $request->all();
         $data['slug'] = Str::slug($data['title'], '-');
+        $slug_exsist=Post::where('slug',$data['slug'])->first();
+        $counter = 0;
+        while($slug_exsist){
+            $title=$data['title'] . "-" . $counter;
+            $data['slug']=Str::slug($title,'-');
+            $slug_exsist=Post::where('slug',$data['slug'])->first();
+            $counter++;
+        }
         $new_post = new Post();
         $new_post->fill($data);
         $new_post->save();
@@ -84,11 +93,24 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Post $post)
+    public function update(PostRequest $request, Post $post)
     {
         $data = $request->all();
         //dd($data);
-        $data['slug'] = Str::slug($post->title, '-');
+        //$data['slug'] = Str::slug($post->title, '-');
+        if($data['slug'] === $post->slug){
+            $data['slug'] = $post->slug;
+        }else{
+            $data['slug']=Str::slug($data['title'],'-');
+            $slug_exsist=Post::where('slug',$data['slug'])->first();
+            $counter = 0;
+            while($slug_exsist){
+                $title=$data['title'] . "-" . $counter;
+                $data['slug']=Str::slug($title,'-');
+                $slug_exsist=Post::where('slug',$data['slug'])->first();
+                $counter++;
+            }
+        }
         $post->update($data);
         return redirect()->route('admin.posts.show',$post);
     }
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,6 +3,16 @@
 @section('content')
 <div class="container">
     <h1>Nuovo post</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+        
+    @endif
     <div>
         <form action="{{ route('admin.posts.store') }}" method="POST">
             @csrf
@@ -10,12 +20,18 @@
 
             <div class="mb-3">
                 <label class="label-control" for="title">Titolo</label>
-                <input type="text" id="title" name="title"  class="form-control">
+                <input type="text" id="title" name="title"  class="form-control @error('title') is-invalid @enderror" value="{{old('title')}}">
+                @error('title')
+                    <div class="text-danger">{{$message}}<div>                   
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="label-control" for="content">Content</label>
-                <textarea type="text" id="content" name="content" class="form-control" rows="5" ></textarea>
+                <textarea type="text" id="content" name="content" class="form-control @error('content') is-invalid @enderror" rows="5" >{{old('content')}}</textarea>
+                @error('content')
+                    <div class="text-danger">{{$message}}<div>                   
+                @enderror
             </div>
             <div>
                 <button class="btn btn-primary" type="submit">Invio</button>
